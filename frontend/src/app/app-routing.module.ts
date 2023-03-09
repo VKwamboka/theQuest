9a9a05d07ecd7d03557e5d26f8263d1abbe7be00
Verah Kwamboka <verahnyagaka101@gmail.com>
2023-03-09 17:46:38 +0300

    updated landing page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  
   {path:'', loadComponent:()=>import('./landing-page/landing-page.component').then(l=>l.LandingPageComponent)},
+  {path:'login', loadComponent:()=>import('./core/login/login.component').then(l=>l.LoginComponent)},
 ];
 
 @NgModule({
