c8365f6f2ef5bcc687c549475413c650c8061403
Verah Kwamboka <verahnyagaka101@gmail.com>
2023-03-09 23:08:10 +0300

    updated code

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   
   {path:'', loadComponent:()=>import('./landing-page/landing-page.component').then(l=>l.LandingPageComponent)},
   {path:'login', loadComponent:()=>import('./core/login/login.component').then(l=>l.LoginComponent)},
+  {path:'register', loadComponent:()=>import('./core/signup/signup.component').then(l=>l.SignupComponent)},
 ];
 
 @NgModule({
