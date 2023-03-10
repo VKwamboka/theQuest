65ec7c05fe6e1a7becb5d7f7adf0cf4328fe2d31
Verah Kwamboka <verahnyagaka101@gmail.com>
2023-03-10 18:15:19 +0300

    updated code

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,7 +6,11 @@ const routes: Routes = [
   {path:'', loadComponent:()=>import('./landing-page/landing-page.component').then(l=>l.LandingPageComponent)},
   {path:'login', loadComponent:()=>import('./core/login/login.component').then(l=>l.LoginComponent)},
   {path:'register', loadComponent:()=>import('./core/signup/signup.component').then(l=>l.SignupComponent)},
-  {path:'user', loadComponent:()=>import('./userdashboard/userdashboard.component').then(l=>l.UserdashboardComponent)},
+  {
+    path:'user', loadComponent:()=>import('./userdashboard/userdashboard.component').then(l=>l.UserdashboardComponent),children:[
+      {path:'profile', loadComponent:()=>import('./userprofile/userprofile.component').then(l=>l.UserprofileComponent)},
+    ]
+  },
   {
     path:'admin', loadComponent:()=>import('./adminashboard/adminashboard.component').then(l=>l.AdminashboardComponent),children:[
       {path:'', loadComponent:()=>import('./admincontent/admincontent.component').then(l=>l.AdmincontentComponent)},
@@ -17,7 +21,6 @@ const routes: Routes = [
     ]
   },
   {path:'question', loadComponent:()=>import('./question/question.component').then(l=>l.QuestionComponent)},
-  {path:'profile', loadComponent:()=>import('./userprofile/userprofile.component').then(l=>l.UserprofileComponent)},
   {path:'editprofile', loadComponent:()=>import('./editprofile/editprofile.component').then(l=>l.EditprofileComponent)},
   {path:'fullquestion', loadComponent:()=>import('./full-question/full-question.component').then(l=>l.FullQuestionComponent)},
   
