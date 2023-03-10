b0a0c1f8c468c50e03b079a68af41eca4b0128c2
Verah Kwamboka <verahnyagaka101@gmail.com>
2023-03-10 10:11:30 +0300

    added user profile

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -9,6 +9,8 @@ const routes: Routes = [
   {path:'user', loadComponent:()=>import('./userdashboard/userdashboard.component').then(l=>l.UserdashboardComponent)},
   {path:'admin', loadComponent:()=>import('./adminashboard/adminashboard.component').then(l=>l.AdminashboardComponent)},
   {path:'question', loadComponent:()=>import('./question/question.component').then(l=>l.QuestionComponent)},
+  {path:'profile', loadComponent:()=>import('./userprofile/userprofile.component').then(l=>l.UserprofileComponent)},
+  {path:'editprofile', loadComponent:()=>import('./editprofile/editprofile.component').then(l=>l.EditprofileComponent)},
 ];
 
 @NgModule({
