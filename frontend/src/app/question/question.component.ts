0f5c9e42a6ce094ae0a5d8bec675fa46d8e22ed4
Verah Kwamboka <verahnyagaka101@gmail.com>
2023-03-11 22:38:01 +0300

    updated code

--- frontend/src/app/question/question.component.ts
+++ frontend/src/app/question/question.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-question',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.css']
 })
