28f4bb571d0127e9750949bc76b38f0acc93c6e0
Verah Kwamboka <verahnyagaka101@gmail.com>
2023-03-13 09:43:09 +0300

    updated carousel

--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -12,5 +12,49 @@ import { FooterComponent } from '../shared/footer/footer.component';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent {
+  
+   questions_img=[
+    'https://cdn.sstatic.net/Img/home/illo-feats-ask.svg?v=b6cd07f0765a',
+    'https://cdn.sstatic.net/Img/home/illo-feats-vote.svg?v=9d2eb0efdc17',
+    'https://cdn.sstatic.net/Img/home/illo-feats-answer.svg?v=b637b99bc32a',
+    'https://cdn.sstatic.net/Img/home/illo-feats-tags.svg?v=0655cbe6bffa',
+    'https://cdn.sstatic.net/Img/home/illo-feats-accept.svg?v=f2be4b8dfdac',
+    'https://cdn.sstatic.net/Img/home/illo-feats-recognize.svg?v=4f011d7173e8'
+]
+objects = [
+  {
+    name: 'Object 1',
+    imageUrl: 'https://cdn.sstatic.net/Img/home/illo-feats-ask.svg?v=b6cd07f0765a'
+  },
+  {
+    name: 'Object 2',
+    imageUrl: 'https://cdn.sstatic.net/Img/home/illo-feats-vote.svg?v=9d2eb0efdc17'
+  },
+  {
+    name: 'Object 3',
+    imageUrl: 'https://cdn.sstatic.net/Img/home/illo-feats-answer.svg?v=b637b99bc32a'
+  },
+  {
+    name: 'Object 4',
+    imageUrl: 'https://cdn.sstatic.net/Img/home/illo-feats-tags.svg?v=0655cbe6bffa'
+  },
+  {
+    name: 'Object 5',
+    imageUrl: 'https://cdn.sstatic.net/Img/home/illo-feats-accept.svg?v=f2be4b8dfdac'
+  },
+  {
+    name: 'Object 6',
+    imageUrl: 'https://cdn.sstatic.net/Img/home/illo-feats-recognize.svg?v=4f011d7173e8'
+  }
+];
 
+selectedObjectIndex!:number;
+
+
+ 
+// question_item_isSelected=false;
+
+
+/*Questions Animation*/
+ 
 }
