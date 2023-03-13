a7c8bd57ff507fcabc87b5f469477c08a3f4d9f4
Verah Kwamboka <verahnyagaka101@gmail.com>
2023-03-13 15:07:22 +0300

    updated carousel

--- frontend/src/app/core/login/login.component.ts
+++ frontend/src/app/core/login/login.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule,FormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  formData = {
+    name: '',
+    email: '',
+    password: ''
+  };
 
+  onSubmit() {
+    // Submit the form data to the server or perform any additional logic here
+  }
 }
