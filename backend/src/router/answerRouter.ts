48b1b266d50340f5f45f37e193efd2a4dd14ed70
Verah Kwamboka <verahnyagaka101@gmail.com>
2023-03-20 07:49:00 +0300

    protected routes

--- backend/src/router/answerRouter.ts
+++ backend/src/router/answerRouter.ts
@@ -1,14 +1,15 @@
 import express from "express";
+import { VerifyToken } from "../middleware/verifyToken";
 import { createAnswer,getAnswers,getAnswerById,updateAnswer,deleteAnswer, markAnswerPreferred } from "../controllers/answer";
 
 const answerRoutes = express.Router();
 
 // answer routes
-answerRoutes.route("/writeAnswer").post(createAnswer);
-answerRoutes.route("/getAnswers").get(getAnswers);
-answerRoutes.route("/getAnswerById/:id").get(getAnswerById);
-answerRoutes.route("/updateAnswer/:id").patch(updateAnswer);
-answerRoutes.route("/deleteAnswer/:id").delete(deleteAnswer);
-answerRoutes.route("/markAnswerPreferred/:id").patch(markAnswerPreferred);
+answerRoutes.route("/writeAnswer").post(VerifyToken,createAnswer);
+answerRoutes.route("/getAnswers").get(VerifyToken,getAnswers);
+answerRoutes.route("/getAnswerById/:id").get(VerifyToken,getAnswerById);
+answerRoutes.route("/updateAnswer/:id").patch(VerifyToken,updateAnswer);
+answerRoutes.route("/deleteAnswer/:id").delete(VerifyToken,deleteAnswer);
+answerRoutes.route("/markAnswerPreferred/:id").patch(VerifyToken,markAnswerPreferred);
 
 export default answerRoutes;
--- backend/src/router/questionRouter.ts
+++ backend/src/router/questionRouter.ts
@@ -1,14 +1,15 @@
 import express from "express";
 import{createQuestion, getAllQuestions,getQuestionById,updateQuestion,deleteQuestion,getFullQuestionById} from '../controllers/question'
+import { VerifyToken } from "../middleware/verifyToken";
 
 const questionRoutes = express.Router();
 
 // question routes
-questionRoutes.route("/postQuestion").post(createQuestion);
-questionRoutes.route("/getAllQuestions").get(getAllQuestions);
-questionRoutes.route("/getQuestionById/:id").get(getQuestionById);
-questionRoutes.route("/updateQuestion/:id").patch(updateQuestion);
-questionRoutes.route("/deleteQuestion/:id").delete(deleteQuestion);
-questionRoutes.route("/getFullQuestionById/:id").get(getFullQuestionById);
+questionRoutes.route("/postQuestion").post(VerifyToken, createQuestion);
+questionRoutes.route("/getAllQuestions").get(VerifyToken, getAllQuestions);
+questionRoutes.route("/getQuestionById/:id").get(VerifyToken, getQuestionById);
+questionRoutes.route("/updateQuestion/:id").patch(VerifyToken, updateQuestion);
+questionRoutes.route("/deleteQuestion/:id").delete(VerifyToken, deleteQuestion);
+questionRoutes.route("/getFullQuestionById/:id").get(VerifyToken, getFullQuestionById);
 
 export default questionRoutes;
