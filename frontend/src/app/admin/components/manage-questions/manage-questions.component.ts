db47ca2a5a62e96a4b5ee8c1fbd28d355f69c342
Verah Kwamboka <verahnyagaka101@gmail.com>
2023-03-22 01:33:03 +0300

    updated users

--- frontend/src/app/admin/components/manage-questions/manage-questions.component.ts
+++ frontend/src/app/admin/components/manage-questions/manage-questions.component.ts
@@ -3,7 +3,7 @@ import { RouterModule,Router } from '@angular/router';
 import { QuestionService } from 'src/app/shared/services/question.service';
 import { AuthService } from 'src/app/core/services/auth';
 import { Store } from '@ngrx/store';
-import { getQuestions } from 'src/app/shared/actions/question';
+import { getQuestions ,deleteQuestion} from 'src/app/shared/actions/question';
 import { Observable } from 'rxjs';
 import { Question } from 'src/app/interfaces/question';
 import { myQuestions } from 'src/app/shared/reducers/question';
@@ -13,12 +13,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-manage-questions',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './manage-questions.component.html',
   styleUrls: ['./manage-questions.component.css']
 })
 export class ManageQuestionsComponent {
   questions$!:Observable<Question[]>
+  id!:string
 constructor(public auth:AuthService, private router:Router, private store:Store<AppState>){}
 
 ngOnInit(): void {
@@ -26,6 +27,12 @@ ngOnInit(): void {
 this.questions$ =this.store.select(myQuestions)
 this.store.dispatch(getQuestions())
 
+}
+
+Delete(){
+  
+  this.store.dispatch(deleteQuestion({id:this.id}))
+  this.router.navigate(['/admin/manage-questions'])
 }
 
 }
