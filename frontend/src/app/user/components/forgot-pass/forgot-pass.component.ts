d4dc6d76bd532ba4ee2025241448f0de6fa3c587
Verah Kwamboka <verahnyagaka101@gmail.com>
2023-03-27 23:41:19 +0300

    updated pass

--- frontend/src/app/user/components/forgot-pass/forgot-pass.component.ts
+++ frontend/src/app/user/components/forgot-pass/forgot-pass.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-forgot-pass',
