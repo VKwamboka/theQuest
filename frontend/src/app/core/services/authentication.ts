4fd6169dabfe223f84f7187cabf8a945b359e2a3
Verah Kwamboka <verahnyagaka101@gmail.com>
2023-03-30 23:13:42 +0300

    updated pass

--- frontend/src/app/core/services/authentication.ts
+++ frontend/src/app/core/services/authentication.ts
@@ -28,4 +28,8 @@ export class AuthenticationService {
     deleteUser(id:string):Observable<Message>{
     return this.http.delete<Message>(`http://localhost:5500/auth/deletecompletely/${id}`)
     }
+
+    forgotPassword(email:string):Observable<Message>{
+      return this.http.post<Message>('http://localhost:550/auth/sendemail',email)
+    }
 }
--- frontend/src/app/user/components/forgot-pass/forgot-pass.component.ts
+++ frontend/src/app/user/components/forgot-pass/forgot-pass.component.ts
@@ -9,11 +9,12 @@ import { login, logout } from 'src/app/core/actions/authActions';
 import { AuthService } from 'src/app/core/services/auth';
 import { AuthenticationService } from 'src/app/core/services/authentication';
 import { AuthState } from 'src/app/core/states/authState';
+import { sendEmail } from '../../actions/answer';
 
 @Component({
   selector: 'app-forgot-pass',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule,ReactiveFormsModule, HttpClientModule],
   templateUrl: './forgot-pass.component.html',
   styleUrls: ['./forgot-pass.component.css']
 })
@@ -32,46 +33,16 @@ export class ForgotPassComponent {
   ngOnInit(): void {
     this.form = this.fb.group({
       Email:[null, [Validators.required, Validators.email]],
-      Password:[null, [Validators.required]],
 
-      // Password:[null, [Validators.required, Validators.minLength(8)]],
     }) 
 
   }
 
   submitForm(){
     console.log(this.form.value)
-
-
-    this.store.dispatch(login({userlogged:this.form.value}));
-    this.authentication.loginUser(this.form.value).subscribe(response=>{
-     
-      this.auth.setRole(response.Role)
-      this.auth.setName(response.Name)
-      this.auth.login()
-      localStorage.setItem('token', response.data.token)
-
-      if(response.data.token  && response.Role == 'user'){
-       
-        this.router.navigate(['/user'])
-        console.log(response.Name)
-      }
-      else if(response.data.token && response.Role == 'admin'){
-        this.router.navigate(['/admin'])
-      }
-    
-    },(error)=>{
-      this.error = true;
-      this.errorMessage = error.error.message;
-
-      console.log(error.error.message)
-    })
-    
-    // this.login$.subscribe(state => console.log(state));
-    
-  
-    console.log('hey')
-   
+    // this.store.dispatch(login({userlogged:this.form.value}));
+    this.authentication.forgotPassword(this.form.value)
+    console.log('hey') 
   }
 
   Close(){
